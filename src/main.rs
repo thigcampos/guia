8eadae6befe0b8ff6c5730e2222595695e525b51
Thiago Campos <commit@thigcampos.com>
2025-01-04 09:04:08 -0300

    implement function to get docs from docsets dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use walkdir::WalkDir;
+use std::path::Path;
+
+const DOCSETS_PATH: &str = "docsets/";
+
+fn get_docs_from_docsets(docsets_path: &str) -> Vec<String> {
+    let mut docs = Vec::new();
+
+    for entry in WalkDir::new(docsets_path)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.metadata().map(|m| m.is_dir()).unwrap_or(false)) 
+        {
+            if entry.path() != Path::new(docsets_path) {
+                if let Some(name) = entry.path().file_name() {
+                    docs.push(name.to_string_lossy().to_string());
+                }
+            }
+        }
+    docs
+}
+
 fn main() {
-    println!("Hello, world!");
+    let docs = get_docs_from_docsets(DOCSETS_PATH);
+    for doc in docs {
+        println!("{}", doc);
+    }
 }
